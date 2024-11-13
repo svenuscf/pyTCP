bce2a54ace45f1b7422f22f4a1d577a77acdaf48
Chun Fung Wong <cfwong@cfwongMBP.local>
2024-11-13 22:44:50 +1100

    Update with Python3 syntax.

--- pytcpclient.py
+++ pytcpclient.py
@@ -3,10 +3,9 @@ import sys
 
 def get_constants(prefix):
     """Create a dictionary mapping socket module constants to their names."""
-    return dict( (getattr(socket, n), n)
-                 for n in dir(socket)
-                 if n.startswith(prefix)
-                 )
+    return dict((getattr(socket, n), n)
+                for n in dir(socket)
+                if n.startswith(prefix))
 
 families = get_constants('AF_')
 types = get_constants('SOCK_')
@@ -15,26 +14,26 @@ protocols = get_constants('IPPROTO_')
 # Create a TCP/IP socket
 sock = socket.create_connection(('localhost', 10000))
 
-print >>sys.stderr, 'Family  :', families[sock.family]
-print >>sys.stderr, 'Type    :', types[sock.type]
-print >>sys.stderr, 'Protocol:', protocols[sock.proto]
-print >>sys.stderr
+print('Family  :', families[sock.family], file=sys.stderr)
+print('Type    :', types[sock.type], file=sys.stderr)
+print('Protocol:', protocols[sock.proto], file=sys.stderr)
+print('', file=sys.stderr)
 
 try:
-    
-    # Send data
+    # Send data (encoding the string into bytes)
     message = 'Sent data: 0123456789 ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+    print('sending "{}"'.format(message), file=sys.stderr)
+    sock.sendall(message.encode('utf-8'))  # Encode the message to bytes
 
     amount_received = 0
     amount_expected = len(message)
-    
+
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
+        print('received "{}"'.format(data.decode('utf-8')), file=sys.stderr)  # Decode the bytes to string
 
 finally:
-    print >>sys.stderr, 'closing socket'
+    print('closing socket', file=sys.stderr)
     sock.close()
+
--- pytcpserver.py
+++ pytcpserver.py
@@ -7,20 +7,21 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the address given on the command line
 server_address = ('', 10000)
 sock.bind(server_address)
-print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
+print('starting up on {} port {}'.format(*sock.getsockname()), file=sys.stderr)
 sock.listen(1)
 
 while True:
-    print >>sys.stderr, 'waiting for a connection'
+    print('waiting for a connection', file=sys.stderr)
     connection, client_address = sock.accept()
     try:
-        print >>sys.stderr, 'client connected:', client_address
+        print('client connected:', client_address, file=sys.stderr)
         while True:
             data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
+            print('received "{}"'.format(data), file=sys.stderr)
             if data:
                 connection.sendall(data)
             else:
                 break
     finally:
         connection.close()
+
